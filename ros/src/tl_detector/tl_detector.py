41555840afd8d110e89d679940d5661956661a99
Raffael Lorenz <raffael@lora-mbp.local>
2020-12-24 13:57:34 +0100

    finalize version 2

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -126,7 +126,7 @@ class TLDetector(object):
         """
         if(not self.has_image):
             self.prev_light_loc = None
-           return False
+            return False
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
